491b4525b0d5045c58e2753d350c91e6f7435c27
Samuel Folledo <samuelfolledo@gmail.com>
2021-06-13 22:00:13 -0400

    [add] remove unneeded codeds

--- iOS/VaxMe/VaxMe/Helpers/Constants.swift
+++ iOS/VaxMe/VaxMe/Helpers/Constants.swift
@@ -13,8 +13,6 @@ struct Constants {
     struct Views {
         //https://github.com/ninjaprox/NVActivityIndicatorView
         static var indicatorView: NVActivityIndicatorView = NVActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 70, height: 70), type: .ballClipRotateMultiple, color: UIColor.white.withAlphaComponent(0.75), padding: 0.0)
-        //https://github.com/ankurbatham/ABAnimateProgressView
-        static var indicatorWithImageView: ABAnimateProgressView = ABAnimateProgressView()
     }
     
     struct Images {
