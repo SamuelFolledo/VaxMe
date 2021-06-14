da1ce47bd180ee5b47934cc4ed9d3290e1dd7d05
Samuel Folledo <samuelfolledo@gmail.com>
2021-06-14 17:48:52 -0400

    [add] updated todos

--- iOS/VaxMe/VaxMe/SignInUpFlow/SignIn/SignInController.swift
+++ iOS/VaxMe/VaxMe/SignInUpFlow/SignIn/SignInController.swift
@@ -9,11 +9,12 @@ import SnapKit
 
 /*
  TODO #1
- - Build UI
- - Save that orange/pink color to UIColor+Extensions (get the hex/rgb from Vlad)
- - Use String+Extensions.swift (not sure if it still works) to update the email and password validator. If both email and passwords are valid, then enable the Sign In button
- - Create the Forgot Password Controller and push it when button is tapped
- - Eye thing on password to show and unshow the password
+ [] Build UI
+ [] Save that orange/pink color to UIColor+Extensions (get the hex/rgb from Vlad)
+ [] Use String+Extensions.swift (not sure if it still works) to update the email and password validator. If both email and passwords are valid, then enable the Sign In button
+ [] Clean texts for email, names, etc. (e.g. email should not have whitespace at the beginning and end)
+ [] Create the Forgot Password Controller and push it when button is tapped
+ [] Eye thing on password to show and unshow the password
  */
 
 
