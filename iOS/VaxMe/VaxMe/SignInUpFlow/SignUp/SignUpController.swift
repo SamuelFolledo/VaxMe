cdd941fa6a55085832942b4bde1f8c7a55e651da
Samuel Folledo <samuelfolledo@gmail.com>
2021-06-14 14:52:09 -0400

    [add] todos and ui for the sign up controller

--- iOS/VaxMe/VaxMe/SignInUpFlow/SignUp/SignUpController.swift
+++ iOS/VaxMe/VaxMe/SignInUpFlow/SignUp/SignUpController.swift
@@ -8,12 +8,15 @@
 import SnapKit
 
 /*
- TODO #2
- - Build UI
- - Save that orange/pink color to UIColor+Extensions (get the hex/rgb from Vlad)
- - Use String+Extensions.swift (not sure if it still works) to update the email and password validator. If both email and passwords are valid, then enable the Sign In button
- - Eye thing on password to show and unshow the password
- - Also create the view when Terms of Services and Privacy Policy is tapped
+ TODO - Ben #2
+ [] Build UI
+    [] hide/unhide password for password and password2 textfields
+    [] put the stack of textfields in its own view subclass to make it clean and keep VC short
+    [] Use AppService to create these textfields and other views to allow reusability
+ [] Save that orange/pink color to UIColor+Extensions (get the hex/rgb from Vlad)
+ [] Use String+Extensions.swift (not sure if it still works) to update the email and password validator. If both email and passwords are valid, then enable the Sign In button
+ [] Eye thing on password to show and unshow the password
+ [] Also create the view when Terms of Services and Privacy Policy is tapped
  */
 
 
@@ -23,10 +26,132 @@ class SignUpController: UIViewController {
     
     //MARK: Views
     
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+//        textField.placeholder = "Enter email"
+        let placeHolder = NSAttributedString(string: "Enter email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
+        textField.attributedPlaceholder = placeHolder
+        textField.font = .font(size: 18, weight: .medium, design: .rounded)
+        textField.textColor = .black
+        textField.backgroundColor = UIColor(r: 242, g: 242, b: 242, a: 1)
+        textField.setPadding(left: 15, right: 15)
+        textField.layer.cornerRadius = 10
+        textField.textAlignment = .left
+        textField.returnKeyType = .next
+        textField.keyboardType = .emailAddress
+        textField.autocapitalizationType = .none
+        textField.tintColor = .systemPink
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
+    
+    lazy var usernameTextField: UITextField = {
+        let textField = UITextField()
+//        textField.placeholder = "Enter username"
+        let placeHolder = NSAttributedString(string: "Enter username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
+        textField.attributedPlaceholder = placeHolder
+        textField.font = .font(size: 18, weight: .medium, design: .rounded)
+        textField.textColor = .black
+        textField.backgroundColor = UIColor(r: 242, g: 242, b: 242, a: 1)
+        textField.setPadding(left: 15, right: 15)
+        textField.layer.cornerRadius = 10
+        textField.textAlignment = .left
+        textField.returnKeyType = .next
+        textField.keyboardType = .default
+        textField.autocapitalizationType = .words
+        textField.tintColor = .systemPink
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+//        textField.placeholder = "Password"
+        let placeHolder = NSAttributedString(string: "Enter password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
+        textField.attributedPlaceholder = placeHolder
+        textField.font = .font(size: 18, weight: .medium, design: .rounded)
+        textField.textColor = .black
+        textField.backgroundColor = UIColor(r: 242, g: 242, b: 242, a: 1)
+        textField.setPadding(left: 15, right: 15)
+        textField.layer.cornerRadius = 10
+        textField.textAlignment = .left
+        textField.returnKeyType = .next
+        textField.keyboardType = .default
+        textField.isSecureTextEntry = true //TODO: - Ben Set this base on the Hide and Unhide feature
+        textField.autocapitalizationType = .none
+        textField.tintColor = .systemPink
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
+    
+    lazy var password2TextField: UITextField = {
+        let textField = UITextField()
+//        textField.placeholder = "Retype password"
+        let placeHolder = NSAttributedString(string: "Retype password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
+        textField.attributedPlaceholder = placeHolder
+        textField.font = .font(size: 18, weight: .medium, design: .rounded)
+        textField.textColor = .black
+        textField.backgroundColor = UIColor(r: 242, g: 242, b: 242, a: 1)
+        textField.setPadding(left: 15, right: 15)
+        textField.layer.cornerRadius = 10
+        textField.textAlignment = .left
+        textField.returnKeyType = .next
+        textField.keyboardType = .default
+        textField.isSecureTextEntry = true //TODO: - Ben Set this base on the Hide and Unhide feature
+        textField.autocapitalizationType = .none
+        textField.tintColor = .systemPink
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
+    
+    var iAgreeToLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = .font(size: 12, weight: .regular, design: .default)
+        label.textColor = .label
+        label.numberOfLines = 2
+        label.textAlignment = .left
+        label.text = "## TODO: with check button for. I agree to Terms of Services and Privacy Poicy"
+        return label
+    }()
+    
+    lazy var continueButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = false
+        button.frame = CGRect(x: 0, y: 0, width: 85, height: 45)
+        button.setTitleColor(.systemBackground, for: .normal)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = .systemPink
+        button.setTitle("Continue", for: .normal)
+        button.addTarget(self, action: #selector(continueButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var signInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = false
+        button.frame = CGRect(x: 0, y: 0, width: 85, height: 45)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        button.setTitleColor(.systemPink, for: .normal)
+        button.setTitle("Sign In", for: .normal)
+        button.addTarget(self, action: #selector(continueButtonTapped), for: .touchUpInside)
+        //add border
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.systemPink.withAlphaComponent(0.9).cgColor
+        button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        button.clipsToBounds = true
+        return button
+    }()
+    
     //MARK: - App LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        hideKeyboardOnTap()
     }
 }
 
@@ -34,5 +159,44 @@ class SignUpController: UIViewController {
 extension SignUpController {
     func setupViews() {
         self.title = "Sign Up"
+        view.backgroundColor = .systemBackground
+        let stackView = UIStackView(axis: .vertical, spacing: 16, distribution: .fill, alignment: .center)
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(8)
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview().offset(-32)
+            $0.bottom.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.bottom).offset(-50)
+        }
+        [emailTextField, usernameTextField, passwordTextField, password2TextField, iAgreeToLabel, continueButton, signInButton
+        ].forEach {
+            stackView.addArrangedSubview($0)
+            $0.snp.makeConstraints{
+                $0.width.equalToSuperview()
+            }
+        }
+        emailTextField.snp.makeConstraints {
+            $0.height.equalTo(30)
+        }
+        usernameTextField.snp.makeConstraints {
+            $0.height.equalTo(emailTextField.snp.height)
+        }
+        passwordTextField.snp.makeConstraints {
+            $0.height.equalTo(emailTextField.snp.height)
+        }
+        password2TextField.snp.makeConstraints {
+            $0.height.equalTo(emailTextField.snp.height)
+        }
+        continueButton.snp.makeConstraints {
+            $0.height.equalTo(40)
+        }
+        signInButton.snp.makeConstraints {
+            $0.height.equalTo(continueButton.snp.height)
+        }
+    }
+    
+    //MARK: @OBJC func
+    @objc func continueButtonTapped() {
+        print("Continue Sign up todo - Samuel")
     }
 }
